83f5bcc5dc51ee7fd8cf98cb2385a99cfdcf2d42
AshinZ <46918019+AshinZ@users.noreply.github.com>
2020-12-07 10:03:09 +0800

    delete database infomation

--- backend/database.php
+++ backend/database.php
@@ -1,8 +1,8 @@
 <?php
-	$server="localhost";//主机
-		$db_username="web";//你的数据库用户名
-		$db_password="xz181115";//你的数据库密码
-		$con = mysqli_connect($server,"web",$db_password,$db_username);//链接数据库
+	$server="";//主机
+		$db_username="";//你的数据库用户名
+		$db_password="";//你的数据库密码
+		$con = mysqli_connect($server,"",$db_password,$db_username);//链接数据库
 		if(!$con)
 			{
 				die("cannot connect".mysqli_error());
